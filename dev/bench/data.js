582da43faed806bd1661ecb47d04fc002a937a0b
github-action-benchmark <github@users.noreply.github.com>
2019-12-21 13:53:23 +0000

    add Benchmark (benchmarkjs) benchmark result for 56619e72e4be9ff838d6279054f296d72d1b131e

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1576936403509,
+  "repoUrl": "https://github.com/Bnaya/objectbuffer",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "me@bnaya.net",
+            "name": "Bnaya Peretz",
+            "username": "Bnaya"
+          },
+          "committer": {
+            "email": "me@bnaya.net",
+            "name": "Bnaya Peretz",
+            "username": "Bnaya"
+          },
+          "distinct": true,
+          "id": "56619e72e4be9ff838d6279054f296d72d1b131e",
+          "message": "Use fork for now",
+          "timestamp": "2019-12-21T15:53:23+02:00",
+          "tree_id": "bdf62167016307bbcffd20b79972cf0f1557bef3",
+          "url": "https://github.com/Bnaya/objectbuffer/commit/56619e72e4be9ff838d6279054f296d72d1b131e"
+        },
+        "date": 1576936403162,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "createObjectBuffer with 200 comments",
+            "value": 52.79,
+            "range": "±9.01%",
+            "unit": "ops/sec",
+            "extra": "57 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
