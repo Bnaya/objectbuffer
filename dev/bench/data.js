f2033c85380f75ecec78804349f9adf1c1b73701
github-action-benchmark <github@users.noreply.github.com>
2019-12-21 14:00:31 +0000

    add Benchmark (benchmarkjs) benchmark result for b546a8294e5e6b300000082da37ae9762632c476

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1576936403509,
+  "lastUpdate": 1576936831516,
   "repoUrl": "https://github.com/Bnaya/objectbuffer",
   "entries": {
     "Benchmark": [
@@ -31,6 +31,37 @@ window.BENCHMARK_DATA = {
             "range": "±9.01%",
             "unit": "ops/sec",
             "extra": "57 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@bnaya.net",
+            "name": "Bnaya Peretz",
+            "username": "Bnaya"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b546a8294e5e6b300000082da37ae9762632c476",
+          "message": "Merge pull request #58 from Bnaya/perf-tests\n\nPerf benchmark infra",
+          "timestamp": "2019-12-21T15:59:26+02:00",
+          "tree_id": "bdf62167016307bbcffd20b79972cf0f1557bef3",
+          "url": "https://github.com/Bnaya/objectbuffer/commit/b546a8294e5e6b300000082da37ae9762632c476"
+        },
+        "date": 1576936831122,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "createObjectBuffer with 200 comments",
+            "value": 63.25,
+            "range": "±3.27%",
+            "unit": "ops/sec",
+            "extra": "65 samples"
           }
         ]
       }
