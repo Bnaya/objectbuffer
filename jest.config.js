7886a8b30dc8b34f8a3668df4a2b967325adcf5b
Bnaya Peretz <me@bnaya.net>
2020-07-10 12:37:05 +0300

    yarn 2 and friends

--- jest.config.js
+++ jest.config.js
@@ -133,7 +133,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node"
+  testEnvironment: "node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -148,9 +148,7 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: ["/node_modules/", ".yarn"],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
