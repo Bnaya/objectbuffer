ec6fe34b2d3f82b1cc2517e77b93e2601e1c2fcc
Bnaya Peretz <me@bnaya.net>
2020-10-26 02:21:56 +0200

    wip

--- src/apiTests/bigint.test.ts
+++ src/apiTests/bigint.test.ts
@@ -6,20 +6,21 @@ import { MAX_64_BIG_INT } from "../internal/consts";
 
 describe("bigint tests", () => {
   test("ok bigint", () => {
-    const ob = createObjectBuffer<any>({}, 512, {});
-    expect(memoryStats(ob).used).toMatchInlineSnapshot(`168`);
+    const ob = createObjectBuffer<any>({}, 1024, {});
+    expect(memoryStats(ob).used).toMatchInlineSnapshot(`208`);
 
     ob.bigintNegative = undefined;
     ob.bigintPositive = undefined;
 
-    expect(memoryStats(ob).used).toMatchInlineSnapshot(`360`);
+    const before = memoryStats(ob).used;
+    expect(before).toMatchInlineSnapshot(`464`);
 
     ob.bigintNegative = MAX_64_BIG_INT * BigInt("-1");
     ob.bigintPositive = MAX_64_BIG_INT;
     ob.bigintPositive = undefined;
     ob.bigintPositive = MAX_64_BIG_INT;
 
-    expect(memoryStats(ob).used).toMatchInlineSnapshot(`408`);
+    expect(memoryStats(ob).used).toMatchInlineSnapshot(`528`);
 
     expect(ob).toMatchInlineSnapshot(`
       Object {
@@ -30,20 +31,21 @@ describe("bigint tests", () => {
   });
 
   test("bad bigint", () => {
-    const ob = createObjectBuffer<any>({}, 512, {});
-    expect(memoryStats(ob).used).toMatchInlineSnapshot(`168`);
+    const ob = createObjectBuffer<any>({}, 1024, {});
+    expect(memoryStats(ob).used).toMatchInlineSnapshot(`208`);
 
     ob.bigintNegative = undefined;
     ob.bigintPositive = undefined;
 
-    expect(memoryStats(ob).used).toMatchInlineSnapshot(`360`);
+    const usedBefore = memoryStats(ob).used;
+    expect(usedBefore).toMatchInlineSnapshot(`464`);
 
     ob.bigintNegative = (MAX_64_BIG_INT + BigInt("1")) * BigInt("-1");
     ob.bigintPositive = MAX_64_BIG_INT + BigInt("1");
     ob.bigintPositive = undefined;
     ob.bigintPositive = MAX_64_BIG_INT + BigInt("1");
 
-    expect(memoryStats(ob).used).toMatchInlineSnapshot(`360`);
+    expect(memoryStats(ob).used).toBe(usedBefore);
 
     expect(ob).toMatchInlineSnapshot(`
       Object {
