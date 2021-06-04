ea0857a767ef1b29e8cd153d4601d6ae823fe465
Bnaya Peretz <me@bnaya.net>
2021-06-05 00:01:19 +0300

    wip

--- codesandbox-examples/comlink-demo-sort-array-on-webworker-no-data-copy/src/index.ts
+++ codesandbox-examples/comlink-demo-sort-array-on-webworker-no-data-copy/src/index.ts
@@ -1,24 +1,62 @@
-/* eslint-disable */
+/* eslint-env browser  */
+import {
+  createObjectBuffer,
+  loadObjectBuffer,
+  unstable_replaceUnderlyingArrayBuffer,
+  getUnderlyingArrayBuffer,
+} from "@bnaya/objectbuffer";
+import * as comlink from "comlink";
 
-// please check the console
+async function main() {
+  workerSide();
+  mainThreadSide();
+}
 
-import * as objectbufferModule from "@bnaya/objectbuffer";
-import * as ComlinkModule from "comlink";
+async function mainThreadSide() {
+  const data = await getData();
+  const dataSize = 2 ** 20;
 
-let objectbufferMUMDLocation = "https://unpkg.com/@bnaya/objectbuffer";
-try {
-  // @ts-ignore
-  const objectbufferMaybeUMDOrVersionLocation = require("../package.json")
-    .dependencies["@bnaya/objectbuffer"];
+  const myObjectBuffer = createObjectBuffer(align(dataSize, 8), {
+    posts: data,
+  });
+  console.log("First post before sort", { ...myObjectBuffer.posts[0] });
 
-  // codesandbox ci
-  if (objectbufferMaybeUMDOrVersionLocation.startsWith("https")) {
-    objectbufferMUMDLocation = objectbufferMaybeUMDOrVersionLocation;
-    // simple version
-  } else {
-    objectbufferMUMDLocation = `https://unpkg.com/@bnaya/objectbuffer@${objectbufferMaybeUMDOrVersionLocation}`;
+  const initialArrayBuffer = getUnderlyingArrayBuffer(myObjectBuffer);
+
+  const sortArray = comlink.wrap<(transferWrapper: any) => ArrayBuffer>(
+    comlink.windowEndpoint(window)
+  );
+
+  const arrayBufferBack = await sortArray(
+    comlink.transfer(initialArrayBuffer, [initialArrayBuffer])
+  );
+
+  unstable_replaceUnderlyingArrayBuffer(myObjectBuffer, arrayBufferBack);
+
+  console.log("First post after sort", { ...myObjectBuffer.posts[0] });
+}
+
+// Due to codesandbox limitations with webworker, the worker runs also on the main thread
+function workerSide() {
+  comlink.expose(sortArray, comlink.windowEndpoint(window));
+
+  function sortArray(ab: ArrayBuffer) {
+    const myObjectBufferInWorker = loadObjectBuffer(ab);
+
+    myObjectBufferInWorker.posts.sort((postA: Post, postB: Post) => {
+      if (postA.body > postB.body) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+
+    return comlink.transfer(ab, [ab]);
   }
-} catch {}
+}
+
+main();
+
 function align(value: number, alignTo: number) {
   return value + alignTo - (value % alignTo);
 }
@@ -32,97 +70,9 @@ interface Post {
 }
 
 async function getData() {
-  const data: Post[] = await (await fetch(
-    "https://jsonplaceholder.typicode.com/comments"
-  )).json();
+  const data: Post[] = await (
+    await fetch("https://jsonplaceholder.typicode.com/comments")
+  ).json();
 
   return data;
-}
-
-const externalArgs: objectbufferModule.ExternalArgs = {};
-
-async function main() {
-  const data = await getData();
-  const dataSize = 2 ** 20;
-
-  const myObjectBuffer = objectbufferModule.createObjectBuffer(
-    externalArgs,
-    align(dataSize + 29, 8),
-    {
-      posts: data
-    }
-  );
-  console.log("Initial", { ...myObjectBuffer.posts[0] });
-
-  const initialArrayBuffer = objectbufferModule.getUnderlyingArrayBuffer(
-    myObjectBuffer
-  );
-
-  const sortArray = ComlinkModule.wrap<(transferWrapper: any) => ArrayBuffer>(
-    new Worker(
-      /**
-       *  trick to load worker in sandbpx
-       */
-      `data:application/javascript;base64,${btoa(
-        `(${workerCode.toString()})(${JSON.stringify(
-          objectbufferMUMDLocation
-        )})`
-      )}`,
-      {
-        name: "exampleWorker"
-      }
-    )
-  );
-
-  const arrayBufferBack = await sortArray(
-    ComlinkModule.transfer(initialArrayBuffer, [initialArrayBuffer])
-  );
-
-  objectbufferModule.replaceUnderlyingArrayBuffer(
-    myObjectBuffer,
-    arrayBufferBack
-  );
-
-  console.log("After Sort", { ...myObjectBuffer.posts[0] });
-}
-
-main();
-
-/**
- *  trick to load worker in sandbpx
- */
-function workerCode(localObjectbufferMUMDLocation: string) {
-  importScripts(
-    localObjectbufferMUMDLocation ||
-      "https://unpkg.com/@bnaya/objectbuffer@0.10.0/dist/objectbuffer.umd.js"
-  );
-  importScripts("https://unpkg.com/comlink@4");
-
-  let objectbufferModuleInWorker: typeof import("@bnaya/objectbuffer");
-
-  // @ts-ignore
-  objectbufferModuleInWorker = self.objectbuffer;
-
-  const ComlinkModuleInWorker: typeof import("comlink") = (self as any).Comlink;
-
-  ComlinkModuleInWorker.expose(sortArray);
-
-  function sortArray(ab: ArrayBuffer) {
-    const externalArgs: objectbufferModule.ExternalArgs = {};
-
-    const myObjectBufferInWorker = objectbufferModuleInWorker.loadObjectBuffer(
-      externalArgs,
-      ab
-    );
-
-    myObjectBufferInWorker.posts.sort((postA: Post, postB: Post) => {
-      if (postA.body > postB.body) {
-        return 1;
-      } else {
-        return -1;
-      }
-    });
-
-    return ComlinkModuleInWorker.transfer(ab, [ab]);
-  }
 }
