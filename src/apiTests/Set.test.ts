32cd7071eac3466ded1e4825973282cd49275826
Bnaya Peretz <me@bnaya.net>
2025-08-26 12:25:18 +0300

    Add failing tests for new Set methods

--- src/apiTests/Set.test.ts
+++ src/apiTests/Set.test.ts
@@ -154,3 +154,69 @@ describe("Set tests", () => {
     `);
   });
 });
+
+describe("new set methods", () => {
+  test.failing("union", () => {
+    const objectBuffer = createObjectBuffer<any>(1024, {});
+    objectBuffer.setA = new Set([1, 2, 3]);
+    objectBuffer.setB = new Set([3, 4, 5]);
+
+    const result = objectBuffer.setA.union(objectBuffer.setB);
+    expect([...result]).toMatchInlineSnapshot(`[1, 2, 3, 4, 5]`);
+  });
+
+  test.failing("intersection", () => {
+    const objectBuffer = createObjectBuffer<any>(1024, {});
+    objectBuffer.setA = new Set([1, 2, 3]);
+    objectBuffer.setB = new Set([3, 4, 5]);
+
+    const result = objectBuffer.setA.intersection(objectBuffer.setB);
+    expect([...result]).toMatchInlineSnapshot(`[3]`);
+  });
+
+  test.failing("difference", () => {
+    const objectBuffer = createObjectBuffer<any>(1024, {});
+    objectBuffer.setA = new Set([1, 2, 3]);
+    objectBuffer.setB = new Set([3, 4, 5]);
+
+    const result = objectBuffer.setA.difference(objectBuffer.setB);
+    expect([...result]).toMatchInlineSnapshot(`[1, 2]`);
+  });
+
+  test.failing("symmetricDifference", () => {
+    const objectBuffer = createObjectBuffer<any>(1024, {});
+    objectBuffer.setA = new Set([1, 2, 3]);
+    objectBuffer.setB = new Set([3, 4, 5]);
+
+    const result = objectBuffer.setA.symmetricDifference(objectBuffer.setB);
+    expect([...result]).toMatchInlineSnapshot(`[1, 2, 4, 5]`);
+  });
+
+  test.failing("isSubsetOf", () => {
+    const objectBuffer = createObjectBuffer<any>(1024, {});
+    objectBuffer.setA = new Set([1, 2]);
+    objectBuffer.setB = new Set([1, 2, 3]);
+
+    expect(objectBuffer.setA.isSubsetOf(objectBuffer.setB)).toBe(true);
+    expect(objectBuffer.setB.isSubsetOf(objectBuffer.setA)).toBe(false);
+  });
+
+  test.failing("isSupersetOf", () => {
+    const objectBuffer = createObjectBuffer<any>(1024, {});
+    objectBuffer.setA = new Set([1, 2, 3]);
+    objectBuffer.setB = new Set([1, 2]);
+
+    expect(objectBuffer.setA.isSupersetOf(objectBuffer.setB)).toBe(true);
+    expect(objectBuffer.setB.isSupersetOf(objectBuffer.setA)).toBe(false);
+  });
+
+  test.failing("isDisjointFrom", () => {
+    const objectBuffer = createObjectBuffer<any>(1024, {});
+    objectBuffer.setA = new Set([1, 2, 3]);
+    objectBuffer.setB = new Set([4, 5, 6]);
+    objectBuffer.setC = new Set([3, 4, 5]);
+
+    expect(objectBuffer.setA.isDisjointFrom(objectBuffer.setB)).toBe(true);
+    expect(objectBuffer.setA.isDisjointFrom(objectBuffer.setC)).toBe(false);
+  });
+});
